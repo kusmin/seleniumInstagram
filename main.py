69b89955483cdee08f3f8f06aab80654b58b8876
renan ribeiro <renan.lagee@gmail.com>
2022-09-23 13:32:47 -0300

    teste

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from re import T
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,126 +15,48 @@ import random
 
 
 class Boot:
-    def __init__(self, username, password):
-
-        self.username = username
-        self.password = password
+    def __init__(self):
         options = Options()
+        # options.add_argument("--user-data-dir=/home/renan/.config/google-chrome")
+
+        # options.add_argument("--profile-directory=/home/renan/.config/google-chrome")
         # options.add_argument("headless")
+        print(options.arguments)
+
         self.browser = webdriver.Chrome(
             options=options, executable_path='./chromedriver')
         self.browser.maximize_window()
 
     def login(self):
         browser = self.browser
-        browser.get("https://www.instagram.com/")
-        try:
-            time.sleep(5)
-            login_button = browser.find_element_by_xpath(
-                "//a[@href='/accounts/login/?source=auth_switcher']"
-            )
-            login_button.click()
-        except:
-            print('já estamos na página de login')
-            pass
-        user_element = browser.find_element_by_xpath(
-            "//input[@name='username']")
-        user_element.clear()
-        time.sleep(random.randint(4, 6))
-        user_element.send_keys(self.username)
-        time.sleep(random.randint(4, 6))
-        password_element = browser.find_element_by_xpath(
-            "//input[@name='password']")
-        password_element.clear()
-        password_element.send_keys(self.password)
-        time.sleep(random.randint(4, 6))
-        password_element.send_keys(Keys.RETURN)
-        try:
-            time.sleep(random.randint(4, 6))
-            ativar_notificacao = browser.find_element(By.XPATH,
-                                                      "/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[3]/button[2]")
-            ativar_notificacao.click()
-            time.sleep(5)
-        except Exception as e:
-            print(e)
-            pass
+        browser.get("https://web.whatsapp.com")
+        print("Escanei o QR code")
+        input()
+        print("Logado")
 
-    def curtir_fotos_com_a_hastag(self, hashtag, seguir_hastag, paginas, curtiu, seguiu):
+    def send_message(self, numero, message):
         browser = self.browser
-        browser.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
-        time.sleep(5)
-        # if seguir_hastag:
-        #     try:
-        #         browser.find_element(By.PARTIAL_LINK_TEXT, "Seguir").click()
-        #     except Exception as e:
-        #         print(e)
-        #         time.sleep(5)
-
-        for i in range(
-                1, paginas
-        ):  # Altere o segundo valor aqui para que ele desça a quantidade de páginas que você quiser: quer que ele desça 5 páginas então você deve alterar de range(1,3) para range(1,5)
-            browser.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(3)
-        hrefs = browser.find_elements_by_tag_name("a")
-        pic_hrefs = [elem.get_attribute("href") for elem in hrefs]
-        print(hashtag + " fotos: " + str(len(pic_hrefs)))
-        testes = [
-            href
-            for href in pic_hrefs
-            if hashtag in href and href.index("https://www.instagram.com/p") != -1
-        ]
-
-        for pic_href in pic_hrefs:
-            try:
-                pic_href.index("https://www.instagram.com/p")
-            except ValueError as err:
-                print("pulando link inválido")
-                continue
-            browser.get(pic_href)
-            browser.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);")
-            try:
-                time.sleep(random.randint(10, 12))
-                if curtiu:
-                    curtir = browser.find_element(By.XPATH,
-                                                  "//span[@class='_aamw']//button")
-                    curtir.click()
-                if seguiu:
-                    browser.find_element(By.XPATH, "//div[@class='_aacl _aaco _aacw _aad6 _aade']").click()
-
-                time.sleep(random.randint(25, 27))
-            except Exception as e:
-                print(e)
-                time.sleep(5)
+        # browser.get("https://api.whatsapp.com/send?={}&text={}".format(numero, message))
+        # Alert(browser).accept()
+        inp_xpath_search = "//input[@title='Search or start new chat']"
+        input_box_search = WebDriverWait(browser, 50).until(
+            lambda driver: browser.find_element(By.XPATH, inp_xpath_search))
+        input_box_search.click()
+        time.sleep(2)
+        input_box_search.send_keys(browser)
+        time.sleep(2)
+        selected_contact = browser.find_element(By.XPATH, "//span[@title=' darling ']")
+        selected_contact.click()
+        inp_xpath = '//div[@class="_2S1VP copyable-text selectable-text"][@contenteditable="true"][@data-tab="1"]'
+        input_box = browser.find_element(By.XPATH, inp_xpath)
+        time.sleep(2)
+        input_box.send_keys(browser + Keys.ENTER)
+        time.sleep(2)
 
 
-def defini_hastag():
-    hastag = input(' Deseja curtir ou seguir alguma hastg especifica ? 1- sim 2- Não\n')
-    if hastag == '1':
+numero = '553189791428'
+message = 'Mensagem enviada'
 
-        while True:
-            opcao = input("Qual hastag deseja ? \n")
-            if not opcao:
-                print("Escreva alguma coisa\n")
-            else:
-                break
-                
-        executar.curtir_fotos_com_a_hastag(opcao, 50, True, True, True)
-    elif hastag == '2':
-        print(hastag)
-    else:
-        print("opção invalidas, tente novamente\n")
-        defini_hastag()
-
-nome = input(
-    'Informe os dados para login: \n')
-
-senha = input('Informe a senha de login: \n')
-
-
-executar = Boot(nome, senha)
+executar = Boot()
 executar.login()
-defini_hastag()
-
-
+executar.send_message(numero, message)
